6bd1ace1eeed1912f8f1f8516a81b1c16edd874b
JanekDev <m.wilinski11@gmail.com>
2022-03-14 10:14:02 +0100

    init

---
+++ main.c
@@ -0,0 +1,22 @@
+/*
+==============================================
+                INI PARSER
+
+
+
+
+
+==============================================
+*/
+
+#include <stdio.h>
+
+struct section {
+    char *name;
+    struct key *keys;
+};
+
+int main(){
+
+    return 0;
+}
