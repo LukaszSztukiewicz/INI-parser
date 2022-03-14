cfcd7d799d4f867da4e022561784b999199a36e7
Łukasz Sztukiewicz <sztukiewicz.lukasz@gmail.com>
2022-03-14 19:02:24 +0100

    initial_changes

--- main.c
+++ main.c
@@ -9,74 +9,136 @@ Input formats:
     [section] -> [keyke]
 
 Mode 1 - expr eval
-Mode 2 - section.key - only one, 
+Mode 2 - section.key - only one,
 
 
 ==============================================
 */
 
 #include <stdio.h>
-struct Key{
+struct Key
+{
     char *key;
     char *value;
     struct Key *nextkey;
 };
-struct Section {
+struct Section
+{
     char *name;
     struct Key *keys;
     struct Section *nextsection;
 };
 
-struct Key *firstKey(){
-    struct Key *firstKey = (struct Key *)malloc(sizeof(struct Key));
-    struct Key *nextKey = firstKey()
-    while(fgets(buf, sizeof buf, file) != NULL){
-                if(buf[0] == '['){
-                    return firstKey;
-                }
-            }
+struct Key *firstKey()
+{
+    struct Key *firstKey = malloc(sizeof(struct Key));
+    struct Key *nextKey = firstKey();
+    while (fgets(buf, sizeof buf, file) != NULL)
+    {
+        if (buf[0] == '[')
+        {
+            return firstKey;
+        }
+    }
     firstKey->key = key;
     firstKey->value = value;
     firstKey->nextkey = NULL;
     return firstKey;
 }
-//define parse function returning pointer to struct section
-struct section *parse_file(FILE *file){
+// define parse function returning pointer to struct section
+struct section *parse_file(FILE *file)
+{
     char *buf = NULL;
     struct section *sections = NULL;
-    while(fgets(buf, sizeof buf, file) != NULL){
-        if(buf[0] == '['){
-            //section
+    while (fgets(buf, sizeof buf, file) != NULL)
+    {
+        if (buf[0] == '[')
+        {
+            // section
             struct Section *section = malloc(sizeof(struct Section));
             section->name = strtok(buf, "]");
-            section->keys = NULL;
+            section->keys = firstKey();
             section->nextsection = NULL;
             struct Section *lastsection = section;
-            
         }
     }
     return sections;
-    
+}
+// define parse function returning pointer to struct section
+char *argv_validation(FILE *file)
+{
+    char *buf = NULL;
+    struct section *sections = NULL;
+    while (fgets(buf, sizeof buf, file) != NULL)
+    {
+        if (buf[0] == '[')
+        {
+            // section
+            struct Section *section = malloc(sizeof(struct Section));
+            section->name = strtok(buf, "]");
+            section->keys = firstKey();
+            section->nextsection = NULL;
+            struct Section *lastsection = section;
+        }
+    }
+    return sections;
 }
 
-int main(int argc, char *argv[]){
+char *read_value_from_section(char *first_section, char *section, char *key)
+{
+}
+
+char *get_section(char *state)
+{
+}
+
+char *get_key(char *state)
+{
+}
+
+int main(int argc, char *argv[])
+{
     char *path = argv[1];
     char *state = argv[2];
     FILE *file = fopen(path, "r");
-    if(file == NULL){
+    if (file == NULL)
+    {
         printf("File not found\n");
-        return NULL;
+        return 1;
     }
-    struct Section *parsed_file = parse_file(file);
-    if(*state == 'expression'){
+    struct Section *first_section = parse_file(file);
+
+    // handling expression input
+    if (*state == 'expression')
+    {
         char *expression = argv[3];
+        char *validation_result = argv_validation(expression);
+        if (validation_result == 'int')
+        {
+            // to implement
+        }
+        else if (validation_result == 'string')
+        {
+            // to implement
+        }
+        else if (validation_result == 'invalid')
+        {
+            printf("Invalid arguments for program: %s\n", expression);
+            return 1;
+        }
+        else
+        {
+            printf("Unexpected return from argv_validation function: %s\n", validation_result);
+            return 1;
+        }
     }
-    else{
-        
+    // handling section.key input
+    else
+    {
+        char *desired_section = get_section(state);
+        char *desired_key = get_key(state);
+        char *result = read_value_from_section(first_section, desired_section, desired_key);
+        printf("%s", result);
     }
-
-    
-    
-
     return 0;
 }
