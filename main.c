83794c1dac241a30a26966b2d6410be19583757d
Łukasz Sztukiewicz <sztukiewicz.lukasz@gmail.com>
2022-04-08 00:24:23 +0200

    cosmetic changes and commented code

--- main.c
+++ main.c
@@ -51,8 +51,10 @@ bool is_integer_value(char *value) {
   return true;
 }
 void free_mem(struct Section *first_section) {
+  // traverse all sections
   while (first_section != NULL) {
     free(first_section->name);
+    // traverse all keys in section
     while (first_section->keys != NULL) {
       free(first_section->keys->key);
       free(first_section->keys->value);
@@ -73,6 +75,7 @@ struct Section *parse_file(FILE *file) {
   char buffer[1024];
   while (fgets(buffer, 1024, file) != NULL) {
     if (buffer[0] == '[') {
+      // create a new section
       char *section_name = strtok(buffer, "]") + 1;
       if (!is_identifier_valid(section_name)) {
         printf("Invalid section identifier \"%s\" in INI file\n", section_name);
@@ -85,16 +88,18 @@ struct Section *parse_file(FILE *file) {
       if (first_section == NULL) {
         first_section = new_section;
       } else {
+        // traverse sections to return pointer to last section
         struct Section *i_section = first_section;
         while (i_section->nextsection != NULL) {
           i_section = i_section->nextsection;
         }
         i_section->nextsection = new_section;
       }
-
     } else if (buffer[0] == '\n') {
       continue;
     } else {
+
+      // create a new key
       char *key = strtok(buffer, " ");
       strtok(NULL, " ");
       char *value = strtok(NULL, "\n");
@@ -108,13 +113,17 @@ struct Section *parse_file(FILE *file) {
       strcpy(new_key->value, value);
       new_key->nextkey = NULL;
 
+      // traverse sections to return pointer to last section
       struct Section *i_section = first_section;
       while (i_section->nextsection != NULL) {
         i_section = i_section->nextsection;
       }
+      // if section doesnt have keys yet, add key to section
       if (i_section->keys == NULL) {
         i_section->keys = new_key;
-      } else {
+      }
+      // traverse keys to return pointer to last key
+      else {
         struct Key *i_key = i_section->keys;
         while (i_key->nextkey != NULL) {
           i_key = i_key->nextkey;
@@ -123,11 +132,13 @@ struct Section *parse_file(FILE *file) {
       }
     }
   }
+
   return first_section;
 }
 
 char *read_value_from_section(struct Section *first_section, char *section, char *key) {
 
+  // traverse sections
   struct Section *i_section = first_section;
   while (strcmp(i_section->name, section) != 0) {
     if (i_section->nextsection == NULL)
@@ -135,6 +146,7 @@ char *read_value_from_section(struct Section *first_section, char *section, char
     i_section = i_section->nextsection;
   }
 
+  // traverse keys
   struct Key *i_key = i_section->keys;
   while (strcmp(i_key->key, key) != 0) {
     if (i_key->nextkey == NULL)
@@ -146,6 +158,7 @@ char *read_value_from_section(struct Section *first_section, char *section, char
 }
 
 char *parse_expression(struct Section *parsed_sections, char *expression) {
+  // parse expression in quotes to variables
   char *first_argument_section  = strtok(expression, " .");
   char *first_argument_key      = strtok(NULL, " .");
   char *first_value             = read_value_from_section(parsed_sections, first_argument_section, first_argument_key);
@@ -156,7 +169,11 @@ char *parse_expression(struct Section *parsed_sections, char *expression) {
   bool is_int_first_value       = is_integer_value(first_value);
   bool is_int_second_value      = is_integer_value(second_value);
   char *message                 = malloc(sizeof(char) * (strlen(first_value) + strlen(second_value)) + 1);
+
   if (is_int_first_value && is_int_second_value) {
+
+    // perform operations on integer values
+    // long type is required since multiplication operation can be also performed
     long first_int_value  = atol(first_value);
     long second_int_value = atol(second_value);
     switch (*operand) {
@@ -174,9 +191,16 @@ char *parse_expression(struct Section *parsed_sections, char *expression) {
       break;
     }
     strcpy(message, "");
-  } else if ((is_int_first_value && !is_int_second_value) || (!is_int_first_value && is_int_second_value)) {
+  }
+
+  // check if the values have same type
+  else if ((is_int_first_value && !is_int_second_value) || (!is_int_first_value && is_int_second_value)) {
     strcpy(message, "Error:invalid-operation type mismatch");
-  } else {
+  }
+
+  // both values must be strings, so perform operation on strings
+  else {
+    // only concatenation is allowed
     if (*operand != '+') {
       strcpy(message, "Error:invalid-operation on string");
     } else {
@@ -189,20 +213,20 @@ char *parse_expression(struct Section *parsed_sections, char *expression) {
 
 int main(int argc, char *argv[]) {
   char *path = argv[1];
+
+  // read file
   FILE *file = fopen(path, "r");
   if (file == NULL) {
     printf("File not found\n");
     return 1;
   }
   struct Section *first_section = parse_file(file);
-  if (first_section == NULL) {
-    return 1;
-  }
-
   fclose(file);
 
   // Mode 1
   if ((strcmp(argv[2], "expression") == 0)) {
+
+    // in this mode there must be exactly 3 arguments
     if (argc != 4) {
       printf("Bad number of arguments\n");
       free_mem(first_section);
@@ -214,9 +238,15 @@ int main(int argc, char *argv[]) {
 
   // Mode2
   else {
+
+    // parse section and key identifiers
     char *desired_section = strtok(argv[2], ".");
     char *desired_key     = strtok(NULL, ".");
-    char *result          = read_value_from_section(first_section, desired_section, desired_key);
+
+    // perform read operation
+    char *result = read_value_from_section(first_section, desired_section, desired_key);
+
+    // process the result
     if (strcmp("Error:section-not-found", result) == 0) {
       printf("Failed to find section [%s]", desired_section);
       free_mem(first_section);
